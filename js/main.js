3b294a67a742ff2ca399ffb3be0cdeef23c5a946
Your Name <dan.weibren@gmail.com>
2025-04-24 12:58:12 +0300

    Create function of switching between two main galleries.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+
+function onInit() {
+
+}
+
+function onChooseImg(el) {
+    // document.querySelector('.main-gallery').classList.add('hidden');
+    // document.querySelector('.meme-editor').classList.remove('hidden');
+    document.querySelectorAll('.container').forEach(el => {
+        if (el.classList.contains('hidden')) el.classList.remove('hidden')
+        else el.classList.add('hidden')
+    })
+}
