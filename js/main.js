b16b8fb6cd2d4559b2ffd3df3cedba04ddfdce54
Your Name <dan.weibren@gmail.com>
2025-04-24 13:24:30 +0300

    First level styling for the meme creator.

--- js/main.js
+++ js/main.js
@@ -1,15 +1,46 @@
 'use strict';
 
+var gElCanvas
+var gCtx
+//  Lists 
 
 function onInit() {
+        gElCanvas = document.querySelector('canvas');
+        gCtx = gElCanvas.getContext('2d')
+        
+        renderCanvas()    
+}
 
+function renderCanvas() {
+    reSizeCanvas()
+    gCtx.fillStyle = "white"
+    gCtx.fillRect(0, 0, gElCanvas.width, gElCanvas.height)
+}
+
+function reSizeCanvas() {
+    const elContainer = document.querySelector('.canvas-container');
+    console.log("🚀 ~ reSizeCanvas ~ elContainer:", elContainer)
+    gElCanvas.width = elContainer.offsetWidth - 10;
+    gElCanvas.height = elContainer.offsetHeight - 10;
 }
 
 function onChooseImg(el) {
-    // document.querySelector('.main-gallery').classList.add('hidden');
-    // document.querySelector('.meme-editor').classList.remove('hidden');
     document.querySelectorAll('.container').forEach(el => {
         if (el.classList.contains('hidden')) el.classList.remove('hidden')
         else el.classList.add('hidden')
     })
 }
+
+// Create
+
+
+
+// Read
+
+
+
+// Update
+
+
+
+// Delete
