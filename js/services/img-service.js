3968d449e8ab9c65a6486ed4c1fdeda160660a60
Your Name <dan.weibren@gmail.com>
2025-04-25 14:34:34 +0300

    Create image service and update .html

---
+++ js/services/img-service.js
@@ -0,0 +1,24 @@
+var gImg 
+
+function getGImg(){
+    return gImg;
+}
+
+// function saveMemeImg(imgSrc) {
+//     const memeImg = {
+//         src: imgSrc,
+//     }
+//     gImg.push(memeImg)
+//     console.log("🚀 ~ saveMemeImg ~ gImg:", gImg)
+// }
+
+// function loadImageToCanvas(imgSrc, onImageReady) {
+//     const img = new Image()
+//     img.onload = () => {
+//         onImageReady(img)
+//     }
+//     img.src = imgSrc
+//     gImg = img 
+//     console.log("🚀 ~ loadImageToCanvas ~ gImg:", gImg)
+// }
+
