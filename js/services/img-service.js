725252e1add53dda1b5d5e5e355dcaba5f932140
Your Name <dan.weibren@gmail.com>
2025-04-25 16:29:27 +0300

    Fixed totally the issue with loading a picture .right now picture rendering works good.

--- js/services/img-service.js
+++ js/services/img-service.js
@@ -4,6 +4,36 @@ function getGImg(){
     return gImg;
 }
 
+// function onImgInput(ev) {
+//     loadImageFromInput(ev, renderImg)
+// }
+
+function saveImgObject(el) {
+        const img = new Image()
+        img.onload = () => {
+            gImg = img
+            renderImg(img)
+        }
+        img.src = el.src
+}
+
+// function renderImg(img) {
+//     gElCanvas.height = (img.naturalHeight / img.naturalWidth) * gElCanvas.width
+//     gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height)
+//     reSizeCanvasContainer(gElCanvas.width,gElCanvas.height)
+//     // reSizeCanvas()
+// }
+
+// function loadImageToCanvas(imgSrc, onImageReady) {
+//     const img = new Image()
+//     img.onload = () => {
+//         onImageReady(img)
+//     }
+//     img.src = imgSrc
+//    return gImg.push(img)
+// }
+
+
 // function saveMemeImg(imgSrc) {
 //     const memeImg = {
 //         src: imgSrc,
